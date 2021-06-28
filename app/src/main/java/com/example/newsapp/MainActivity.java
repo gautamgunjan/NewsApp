f6f72be22e96508cfcdfe5a0b53da3988d744530
Gautam Gunjan <1by18cs056@bmsit.in>
2021-06-28 19:02:21 +0530

    first commit

---
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//    ApiKey:92a782d1fe3a401dae7f892d36f23567
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
